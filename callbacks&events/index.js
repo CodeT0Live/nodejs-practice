e41ed050e4b10572c28f1006b6f80707fe48360f
karthik <karthik@Karthiks-Laptop.local>
2023-04-15 23:07:12 +0530

    cps examples

---
+++ callbacks&events/index.js
@@ -0,0 +1,15 @@
+/**
+ * Continuation passing style
+ * 
+ * function ContinuousPassingStyle(param1, param2, callback){
+ *      callback(param1+param2);
+ * }
+ */
+
+/**
+ * Continuous Passing Style Async
+ * 
+ * function addCPS(param1, param2, callback){
+ *      setTimeout(()=> callback(param1,param2));
+ * }
+ */
